a71e8e9cb3f12842026ba1f9584d6dc74f2b3498
yuenzai <80330775@qq.com>
2024-11-12 19:44:02 +0800

    build: modify dependency

--- device/domain/src/main/java/cn/ecosync/ibms/device/model/DevicePointExtra.java
+++
@@ -1,7 +0,0 @@
-package cn.ecosync.ibms.device.model;
-
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", visible = true)
-public interface DevicePointExtra {
-}
