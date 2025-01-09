8c1bd51d5147fe8dd32e10e23a81c665bf4428fc
yuenzai <80330775@qq.com>
2025-01-09 15:24:08 +0800

    ci: add ibms-startup script

---
+++ ibms-startup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+IBMS_HOME=$(pwd)
+
+sudo tee /etc/systemd/system/ibms.service <<EOF
+[Unit]
+Description=ibms
+After=syslog.target network.target
+
+[Service]
+User=metadata
+Group=metadata
+
+Type=exec
+ExecStart=java -jar ${IBMS_HOME}/ibms-bootstrap/target/ibms-bootstrap-0.0.1-SNAPSHOT.jar
+WorkingDirectory=${IBMS_HOME}
+SuccessExitStatus=143
+
+[Install]
+WantedBy=multi-user.target
+EOF
