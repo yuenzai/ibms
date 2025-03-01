756671837f28a591f532bc73b2462b028f8a7490
yuenzai <80330775@qq.com>
2025-03-02 01:09:06 +0800

    chore: modify deploy script

---
+++ install-bacnet-stack.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ ! -f /var/lib/apt/periodic/update-success-stamp ] || [ $(stat -c %Y /var/lib/apt/periodic/update-success-stamp) -lt $(date -d '1 day ago' +%s) ]; then
+  apt-get update || exit 1;
+else
+  echo "apt already updated"
+fi
+
+if ! command -v cmake &> /dev/null || ! command -v git &> /dev/null; then
+  apt-get install -y cmake git || exit 1;
+else
+  echo "cmake and git already installed"
+fi
+
+if [ ! -d cJSON ]; then
+  git clone --branch v1.7.18 https://github.com/DaveGamble/cJSON.git || exit 1;
+else
+  git -C cJSON pull
+fi
+
+if [ ! -d bacnet-stack ]; then
+  git clone --branch 1.3.8 --single-branch https://github.com/yuenzai/bacnet-stack.git || exit 1;
+else
+  git -C bacnet-stack pull
+fi
+
+mkdir -p cJSON/build && \
+cmake -S cJSON -B cJSON/build && \
+make -C cJSON/build install
+
+mkdir -p bacnet-stack/build && \
+cmake -S bacnet-stack -B bacnet-stack/build && \
+make -C bacnet-stack/build clean all
+
+cp bacnet-stack/build/{whois,readpropm,writeprop} /usr/local/bin
--- redeploy.sh
+++ redeploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./mvnw --settings .mvn/wrapper/settings.xml clean package \
-&& docker compose down gateway \
-&& docker compose up -d gateway \
-&& docker compose logs -f --tail 1
+&& docker compose -f ibms-gateway-starter/docker-compose.yml down gateway \
+&& docker compose -f ibms-gateway-starter/docker-compose.yml up -d gateway \
+&& docker compose -f ibms-gateway-starter/docker-compose.yml logs -f --tail 1
